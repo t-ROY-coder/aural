6a404b6e3f01636d983d3fd2926d9c1bc488e7c5
Tanumon Roy <roytanumon@gmail.com>
2021-01-13 20:42:50 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Footer from "./components/Footer";
 import InputImg from "./components/InputImg";
 
 function App() {
   return (
-    <div className="container">
-      <InputImg />
-    </div>
+    <>
+      <div className="container" style={{ minHeight: "100vh" }}>
+        <InputImg />
+      </div>
+      <Footer />
+    </>
   );
 }
 
