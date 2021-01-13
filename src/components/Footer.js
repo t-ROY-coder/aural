a00199bfaf7888e87e6034768c7b201d21658412
Tanumon Roy <roytanumon@gmail.com>
2021-01-13 21:05:14 +0530

    added ion-icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,11 @@ function Footer() {
     <>
       <footer>
         <p>Developed by:</p>
-        <h4>Tanumon Roy</h4>
+        <h4>
+          Tanumon Roy
+          <ion-icon name="logo-github"></ion-icon>
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </h4>
       </footer>
     </>
   );
