f264c5d1b33b8178ab5c0715b4382c0cb6f2588a
Tanumon Roy <roytanumon@gmail.com>
2021-01-21 22:19:01 +0530

    added icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/my-app-logo.png";
 
 function Header() {
   return (
     <>
       <header>
         <Link to="/">
+          <img src={logo} alt="logo" />
           <h3>My-App</h3>
         </Link>
         <ul>
