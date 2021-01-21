6616ee71fd38772541e44b615a97a92feeb653ce
Tanumon Roy <roytanumon@gmail.com>
2021-01-21 20:38:56 +0530

    fixed link style and added anchor for footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,15 @@ function Footer() {
         <p>Developed by:</p>
         <h4>
           Tanumon Roy
-          <ion-icon name="logo-github"></ion-icon>
-          <ion-icon name="logo-linkedin"></ion-icon>
+          <a href="https://github.com/t-ROY-coder" target="_blank">
+            <ion-icon name="logo-github"></ion-icon>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tanumon-roy-6b2109193/"
+            target="_blank"
+          >
+            <ion-icon name="logo-linkedin"></ion-icon>
+          </a>
         </h4>
       </footer>
     </>
